1cf5f41723b9181b38f00b2a1669fda6e36f52a9
OoogleBoogle <simattfield@gmail.com>
2016-07-25 17:48:50 -0700

    Now with explosions!!!

--- src/js/projectile.collision.js
+++ src/js/projectile.collision.js
@@ -10,19 +10,24 @@ function projectileCollision({projectile, tileSize, levels}) {
 
     if(projectile.xVelocity>0){
         if((levels.map[baseRow][baseCol+1] && !levels.map[baseRow][baseCol]) || (levels.map[baseRow+1][baseCol+1] && !levels.map[baseRow+1][baseCol] && rowOverlap)){
-
+            console.log(projectile);
+            projectile.image.src = './img/fireball_die_right.png';
+            projectile.width = 64;
+            projectile.height = 64;
+            projectile.y -= 5;
             projectile.x=baseCol*tileSize;
-            projectile.active = false;
-
+            setTimeout(() => { projectile.active = false }, 100) ;
         }
     }
 
     if(projectile.xVelocity<0){
         if((!levels.map[baseRow][baseCol+1] && levels.map[baseRow][baseCol]) || (!levels.map[baseRow+1][baseCol+1] && levels.map[baseRow+1][baseCol] && rowOverlap)){
-
+            projectile.image.src = './img/fireball_die_left.png';
+            projectile.width = 64;
+            projectile.height = 64;
+            projectile.y -= 5;
             projectile.x=(baseCol+1)*tileSize;
-            projectile.active = false;
-
+            setTimeout(() => { projectile.active = false }, 100);
         }
     }
 
@@ -30,18 +35,24 @@ function projectileCollision({projectile, tileSize, levels}) {
 
     if(projectile.yVelocity>0){
         if((levels.map[baseRow+1][baseCol] && !levels.map[baseRow][baseCol]) || (levels.map[baseRow+1][baseCol+1] && !levels.map[baseRow][baseCol+1] && colOverlap)){
-
+            projectile.image.src = './img/fireball_die_down.png';
+            projectile.width = 64;
+            projectile.height = 64;
+            projectile.x -= 2;
             projectile.y = baseRow*tileSize;
-            projectile.active = false;
+            setTimeout(() => { projectile.active = false }, 100);
 
         }
     }
 
 	if(projectile.yVelocity<0){
 		if((!levels.map[baseRow+1][baseCol] && levels.map[baseRow][baseCol]) || (!levels.map[baseRow+1][baseCol+1] && levels.map[baseRow][baseCol+1] && colOverlap)){
-
+            projectile.image.src = './img/fireball_die_up.png';
+            projectile.width = 64;
+            projectile.height = 64;
+            projectile.x -= 3;
 			projectile.y = (baseRow+1)*tileSize;
-      projectile.active = false;
+            setTimeout(() => { projectile.active = false }, 100);
 		}
 	}
 };
