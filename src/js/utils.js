895ef7c2037f565d571f468ccb6e3bf1be525e01
OoogleBoogle <simattfield@gmail.com>
2016-08-01 15:04:04 -0700

    ...

--- src/js/utils.js
+++ src/js/utils.js
@@ -32,7 +32,10 @@ var init = function() {
     'spec/gaunt.json',
     'spec/sprite.json',
     'spec/tTile.json'
-  ])
+  ]);
+  $.getJSON('spec/farmer.json', function(data) {
+    console.log(data);
+  })
   $(window).on('resize', function() {
     resizeIntro();
   })
